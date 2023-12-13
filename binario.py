463b8e44247dbe28e8080a11cab8b1c6a3728f1f
TonhaoTurbina <lucasarcr@gmail.com>
2023-12-13 11:40:05 -0300

    adicionei resolução de numeros inteiros negativos.

--- binario.py
+++ binario.py
@@ -3,21 +3,25 @@ def obter_numero():
     while True:
         try:
             inteiro = int(input('Digite um número inteiro: '))
-            if inteiro >= 0:
-                return inteiro
-            else:
-                print('Número digitado inválido. Tente novamente.')
+            return inteiro
         except ValueError:
             print('Número digitado inválido. Tente novamente.')
 
 def converte_binario(num):
-    """ Converte o número inteiro para binário. """
-    return bin(num)[2:]
+    """ Converte o número inteiro para binário e adiciona os 8 digitos. """
+    if num >= 0:
+        binario_absoluto = bin(num)[2:]
+        binario_completo = binario_absoluto.zfill(8)
+    else:
+        binario_absoluto = bin(num & ((1 << 8) - 1))[2:]
+        binario_completo = binario_absoluto.zfill(8)
+
+    return binario_completo
 
 def main():
-    inteiro_positivo = obter_numero()
-    binario = converte_binario(inteiro_positivo)
-    print(f'O {inteiro_positivo} em binário é {binario}')
+    inteiro = obter_numero()
+    binario = converte_binario(inteiro)
+    print(f'O {inteiro} em binário é {binario[:4]} {binario[4:]}')
 
 if __name__ == '__main__':
     main()
